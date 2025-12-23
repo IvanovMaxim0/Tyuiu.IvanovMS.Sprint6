9f4833310d276a1fb29d34800b283c554644006e
Tyuiu.IvanovMS@outlook.com <ivanovmaximqq@gmail.com>
2025-12-23 21:07:47 +0500

    Task1

---
+++ Tyuiu.IvanovMS.Sprint6.Task1.V7.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.IvanovMS.Sprint6.Task1.V7.Lib;
+namespace Tyuiu.IvanovMS.Sprint6.Task1.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.IvanovMS.Sprint6.Task1.V7/FormMain.cs
@@ -0,0 +1,53 @@
+using Tyuiu.IvanovMS.Sprint6.Task1.V7.Lib;
+namespace Tyuiu.IvanovMS.Sprint6.Task1.V7
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        DataService ds = new DataService();
+        private void buttonDone_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int startStep = Convert.ToInt32(textBoxStartStep.Text);
+                int stopStep = Convert.ToInt32(textBoxStopStep.Text);
+
+                string strLine;
+
+                int len = ds.GetMassFunction(startStep, stopStep).Length;
+
+                double[] valueArray;
+                valueArray = new double[len];
+
+                valueArray = ds.GetMassFunction(startStep, stopStep);
+                textBoxResult.Text = "";
+                textBoxResult.AppendText("+----------+----------+" + Environment.NewLine);
+                textBoxResult.AppendText("|    X     |    f(x)  |" + Environment.NewLine);
+                textBoxResult.AppendText("+----------+----------+" + Environment.NewLine);
+
+                for (int i = 0; i <= len - 1; i++)
+                {
+                    strLine = String.Format("|{0,5:d}     |  {1, 5:f2}   |", startStep, valueArray[i]);
+                    textBoxResult.AppendText(strLine + Environment.NewLine);
+                    startStep++;
+                }
+
+                textBoxResult.AppendText("+----------+----------+" + Environment.NewLine);
+
+            }
+            catch
+            {
+                MessageBox.Show("Введены неверные данные", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+        private void buttonHelp_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Таск 1 выполнил студент группы ПКТБ-25-1 Иванов Максим Сергеевич", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
